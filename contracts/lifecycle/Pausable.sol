fe50fcbf33b6cb6b833bb2422f7f6e2d2088bd3d
João Gabriel Carvalho <jgcarvalho93@gmail.com>
2017-04-29 12:56:46 -0300

    Applied change regarding the styling

--- contracts/lifecycle/Pausable.sol
+++ contracts/lifecycle/Pausable.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.4.8;
 import "../ownership/Ownable.sol";
 
 
-/*
+/**
  * @title Pausable
  * @dev Abstract contract that allows children to implement an emergency stop mechanism.
  */
 contract Pausable is Ownable {
   bool public stopped;
 
-  /*
+  /**
    * @dev modifier that ensure that the contract isn't in a stopped state
    */
   modifier stopInEmergency {
@@ -21,7 +21,7 @@ contract Pausable is Ownable {
     _;
   }
 
-  /*
+  /**
    * @dev modifier that ensure that the contract is in a stopped state
    */
   modifier onlyInEmergency {
@@ -31,14 +31,14 @@ contract Pausable is Ownable {
     _;
   }
 
-  /*
+  /**
    * @dev called by the owner on emergency, triggers stopped state
    */
   function emergencyStop() external onlyOwner {
     stopped = true;
   }
 
-  /*
+  /**
    * @dev called by the owner on end of emergency, returns to normal state
    */
   function release() external onlyOwner onlyInEmergency {
